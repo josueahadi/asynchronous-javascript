d35602cfb9d990275eca5b7d234465d589997a7b
josueahadi <ahadihjosue@gmail.com>
2024-06-13 12:22:19 +0200

    chore: make the code more reusable by putting it inside the getTodos function, call the function to get the todos, pass in a callback a function as an argument and fire it with either the err or data if it's a success or not

--- sandbox.js
+++ sandbox.js
@@ -8,17 +8,27 @@
 // console.log(3)
 // console.log(4)
 
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText);
-    } else if (request.readyState === 4) {
-        console.log('could not fetch the data');
-    
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
+        if (request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4) {
+            callback('could not fetch data', undefined);
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    request.send();
+};
+
+getTodos((err, data) => {
+    //console.log('callback function fired');
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
