47c73c2f21e0a060d31bc3822165d2539ea3468c
josueahadi <ahadihjosue@gmail.com>
2024-06-13 16:37:18 +0200

    callback hell

--- sandbox.js
+++ sandbox.js
@@ -8,7 +8,7 @@
 // console.log(3)
 // console.log(4)
 
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
@@ -21,19 +21,25 @@ const getTodos = (callback) => {
         }
     });
 
-    request.open('GET', './todos.json');
+    request.open('GET', resource);
     request.send();
 };
 
 console.log(1);
 console.log(2);
 
-getTodos((err, data) => {
+getTodos('todos/luigi.json', (err, data) => {
     console.log('callback function fired');
     if(err) {
         console.log(err);
     } else {
         console.log(data);
+        getTodos('todos/mario.json', (err, data) => {
+            console.log(data);
+            getTodos('todos/shaun.json', (err, data) => {
+                console.log(data);
+            });
+        });
     }
 });
 
