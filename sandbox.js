6944c4d344dfba4ac99ef555ef0bce6851c26371
josueahadi <ahadihjosue@gmail.com>
2024-06-14 14:56:02 +0200

    handling errors in asyn functions

--- sandbox.js
+++ sandbox.js
@@ -112,10 +112,16 @@ fetch('todos/luigi.json').then(response => {
 // Async & Await 
 const getTodos = async () => {
     const response = await fetch('todos/luigi.json')
+
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data')
+    }
+
     const data = await response.json();
 
     return data;
 };
 
 getTodos()
-    .then(data => console.log('resolved:', data));
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
